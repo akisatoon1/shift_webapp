83a510cf62811cb7933aa91baa83082a4be50e7b
akisatoon <akisatoon@gmail.com>
2025-03-24 00:49:50 +0900

    implement adminRegisterHandler

--- code/handler.go
+++ code/handler.go
@@ -7,33 +7,6 @@ import (
 	"net/http"
 )
 
-func (app *App) registerHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodPost {
-		userID := r.FormValue("user_id")
-		password := r.FormValue("password")
-		hash := sha256.Sum256([]byte(password))
-		hashedPassword := base64.URLEncoding.EncodeToString(hash[:])
-
-		err := app.createUser(userID, hashedPassword)
-		if err != nil {
-			http.Redirect(w, r, "/register", http.StatusFound)
-			return
-		}
-		http.Redirect(w, r, "/login", http.StatusFound)
-		return
-
-	} else {
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		fmt.Fprintln(w, `
-			<form method="POST">
-				<input type="text" name="user_id" placeholder="User ID"><br>
-				<input type="password" name="password" placeholder="Password"><br>
-				<button type="submit">Register</button>
-            </form>
-		`)
-	}
-}
-
 func (app *App) homeHandler(w http.ResponseWriter, r *http.Request) {
 	cookie, err := r.Cookie("session_id")
 	if err != nil {
@@ -139,6 +112,7 @@ func (app *App) adminHomeHandler(w http.ResponseWriter, r *http.Request) {
 	if isAdmin(role) {
 		fmt.Fprintf(w, `
 		<h1>[Admin]Your user ID is '%v'</h1>
+		<a href="/admin/register">create new user</a><br>
 		<a href="/logout">Logout</a>
 	`, userID)
 	} else {
@@ -148,4 +122,50 @@ func (app *App) adminHomeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *App) adminRegisterHandler(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie("session_id")
+	if err != nil {
+		http.Redirect(w, r, "/login", http.StatusFound)
+		return
+	}
+
+	userID, err := app.getUserIDFromSession(cookie.Value)
+	if err != nil {
+		http.Redirect(w, r, "/login", http.StatusFound)
+		return
+	}
+
+	_, role, err := app.getUser(userID)
+	if err != nil {
+		http.Redirect(w, r, "/login", http.StatusFound)
+		return
+	}
+	if !isAdmin(role) {
+		http.Error(w, "Forbidden", http.StatusForbidden)
+		return
+	}
+
+	if r.Method == http.MethodPost {
+		userID := r.FormValue("user_id")
+		password := r.FormValue("password")
+		hash := sha256.Sum256([]byte(password))
+		hashedPassword := base64.URLEncoding.EncodeToString(hash[:])
+
+		err := app.createUser(userID, hashedPassword)
+		if err != nil {
+			http.Redirect(w, r, "/register", http.StatusFound)
+			return
+		}
+		http.Redirect(w, r, "/admin/home", http.StatusFound)
+		return
+
+	} else {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		fmt.Fprintln(w, `
+			<form method="POST">
+				<input type="text" name="user_id" placeholder="User ID"><br>
+				<input type="password" name="password" placeholder="Password"><br>
+				<button type="submit">Register</button>
+            </form>
+		`)
+	}
 }
