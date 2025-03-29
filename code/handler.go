b0ac5b17b72862a8b8d20295dc838b4dc3274948
akisatoon <akisatoon@gmail.com>
2025-03-30 00:43:47 +0900

    refact: add description comment

--- code/handler.go
+++ code/handler.go
@@ -16,7 +16,10 @@ var (
 	errInvalidUserID   = errors.New("invalid user id")
 )
 
+// /home
+// ユーザーのホーム画面を表示する
 func (app *App) homeHandler(w http.ResponseWriter, r *http.Request) {
+	// check session
 	userID, err := app.getUserIDFromCookie(r)
 	if err != nil {
 		if err == errSessionNotFound {
@@ -27,13 +30,11 @@ func (app *App) homeHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// check role
+	// adminならば、adminのホーム画面にリダイレクトする
 	usr, err := app.getUser(userID)
 	if err != nil {
-		if err == errUserNotFound {
-			redirectWithError(w, r, "/login", "ログインしてください")
-		} else {
-			responseServerError(w)
-		}
+		responseServerError(w)
 		return
 	}
 	if isAdmin(usr.Role) {
@@ -45,7 +46,12 @@ func (app *App) homeHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(w, usr)
 }
 
+// /login
+// 2つの役割がある
+// 1: loginページの表示をする
+// 1: login処理をする
 func (app *App) loginHandler(w http.ResponseWriter, r *http.Request) {
+	// http methodがPOSTならばlogin処理をする
 	if r.Method == http.MethodPost {
 		userID := r.FormValue("user_id")
 		password := r.FormValue("password")
@@ -62,6 +68,9 @@ func (app *App) loginHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		// password check
 		if usr.Password == base64.URLEncoding.EncodeToString(hash[:]) {
+			// passwordが一致するので、sessionを始める
+			// session_idをCookieでクライアントに送信
+			// ホーム画面にリダイレクト
 			sessionID, err := app.createSession(usr.ID)
 			if err != nil {
 				responseServerError(w)
@@ -81,14 +90,16 @@ func (app *App) loginHandler(w http.ResponseWriter, r *http.Request) {
 			redirectWithError(w, r, "/login", "userIDまたはpasswordが間違っています")
 			return
 		}
-
-	} else {
+	} else if r.Method == http.MethodGet {
+		// http methodがGETなので、loginページを表示
 		errorMessage := r.URL.Query().Get("error")
 		tmpl, _ := template.ParseFiles("./html/login.html")
 		tmpl.Execute(w, map[string]string{"Error": errorMessage})
 	}
 }
 
+// /logout
+// logout処理のみ
 func (app *App) logoutHandler(w http.ResponseWriter, r *http.Request) {
 	cookie, err := r.Cookie("session_id")
 	if err != nil {
@@ -147,12 +158,19 @@ func (app *App) adminMiddleware(handler func(http.ResponseWriter, *http.Request,
 	}
 }
 
+// /admin/home
+// adminのホーム画面を表示する
 func (app *App) adminHomeHandler(w http.ResponseWriter, r *http.Request, usr user) {
 	tmpl, _ := template.ParseFiles("./html/admin/home.html")
 	tmpl.Execute(w, usr)
 }
 
+// /admin/register
+// 2つ役割がある
+// 1: ユーザ作成画面を表示
+// 2: adminではないユーザを新しく作成する
 func (app *App) adminRegisterHandler(w http.ResponseWriter, r *http.Request, usr user) {
+	// http methodがPOSTならばユーザを作成する
 	if r.Method == http.MethodPost {
 		userID := r.FormValue("user_id")
 		password := r.FormValue("password")
@@ -166,20 +184,20 @@ func (app *App) adminRegisterHandler(w http.ResponseWriter, r *http.Request, usr
 			} else {
 				responseServerError(w)
 			}
-			// useridが既に存在している場合、/admin/registerにリダイレクトして
-			// エラーメッセージを表示。
-			// 現在はhttp errorでメッセージを表示する
 			return
 		}
 		http.Redirect(w, r, "/admin/users", http.StatusFound)
 		return
 
-	} else {
+	} else if r.Method == http.MethodGet {
+		// http methodがGETなのでユーザ作成画面を表示する
 		tmpl, _ := template.ParseFiles("./html/admin/register.html")
 		tmpl.Execute(w, nil)
 	}
 }
 
+// /admin/users
+// ユーザ管理画面を表示する
 func (app *App) adminUsersHandler(w http.ResponseWriter, r *http.Request, usr user) {
 	users, err := app.getAllUsers()
 	if err != nil {
@@ -191,6 +209,8 @@ func (app *App) adminUsersHandler(w http.ResponseWriter, r *http.Request, usr us
 	tmpl.Execute(w, users)
 }
 
+// /admin/delete
+// ユーザを削除する
 func (app *App) adminDeleteHandler(w http.ResponseWriter, r *http.Request, usr user) {
 	deletedUserID := r.FormValue("user_id")
 	err := app.deleteUser(deletedUserID)
@@ -201,6 +221,7 @@ func (app *App) adminDeleteHandler(w http.ResponseWriter, r *http.Request, usr u
 	http.Redirect(w, r, "/admin/users", http.StatusFound)
 }
 
+// クライアントが認証されていることを確認する
 func (app *App) getUserIDFromCookie(r *http.Request) (string, error) {
 	cookie, err := r.Cookie("session_id")
 	if err != nil {
