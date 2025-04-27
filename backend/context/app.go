0afd10435a7624ba5d44e08c2e945096bfee48e0
akisatoon <akisatoon@gmail.com>
2025-04-28 01:09:52 +0900

    add new context

--- backend/context/app.go
+++ backend/context/app.go
@@ -2,6 +2,11 @@ package context
 
 import "backend/db"
 
+// アプリケーション全体で利用されるデータを管理
 type AppContext struct {
 	DB db.DB
+}
+
+func NewAppContext(db db.DB) *AppContext {
+	return &AppContext{DB: db}
 }
