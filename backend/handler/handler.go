09911486ab5b09ba60858bcb57e483c1143bf881
akisatoon <akisatoon@gmail.com>
2025-05-06 23:36:56 +0900

    modify response error json format

--- backend/handler/handler.go
+++ backend/handler/handler.go
@@ -14,7 +14,9 @@ type Handler struct {
 
 func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if err := h.handlerFn(h.ctx, w, r); err != nil {
-		http.Error(w, err.message, err.code)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(err.code)
+		w.Write([]byte(`{"error": "` + err.message + `"}`))
 	}
 }
 
